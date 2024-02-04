f882f1e4491ca2d807136ef597325816ddbe55ed
Dimas Ardiminda Edia Putra <ardiminda.com>
2024-02-04 21:50:52 +0700

    bismillah first commit

---
+++ bismillah.jl
@@ -0,0 +1,26 @@
+# bismillah
+println("bismillah")
+
+# tipe data
+Nama = "dimas" #string
+Umur = 20 #int
+
+println("nama saya ", Nama, " Umur saya ", Umur)
+
+# operasi artimatika
+angka_1 = 10
+angka_2 = 20
+angka_2 = angka_1 + angka_2
+println("penjumlaha antara 10 dan 20 adalah ",angka_2)
+
+# if else dan input user
+print("masukan angka pertama: ")
+inputAngka  = parse(Int64, readline())
+print("masukan angka kedua: ")
+inputAngka2 = parse(Int64, readline())
+
+if inputAngka > inputAngka2
+    println("a lebih besar dari b")
+else
+    println("a lebih kecil dari b")
+end
